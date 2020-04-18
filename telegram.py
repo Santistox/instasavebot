8b603972226b5c2281d4f82c37720116673a966f
Aleksandr <a-gerasimov85@mail.ru>
2020-04-18 18:52:38 +0300

    some fixes

--- telegram.py
+++ telegram.py
@@ -18,9 +18,8 @@ def help_message(message):
     help_output += 'To download instagram posts, send the post \'s URL to the bot.\n\n'
     help_output += 'To download stories or HD profile photos, send the account\'s ID to the bot.\nExample:\n@google\n\n'
     help_output += 'I\'ve sent you a picture that shows how to copy post URLs 👇'
-    # img = 'help.jpeg'
     bot.send_message(message.chat.id, help_output)
-    # bot.send_photo(message.chat.id, img)
+    bot.send_photo(message.chat.id, open('help.jpeg', 'rb'))
 
 
 @bot.message_handler(commands=['status'])
@@ -35,12 +34,15 @@ def status_message(message):
     bot.send_message(message.chat.id, status_output)
 
 
+@bot.message_handler(commands=['youtube'])
+def youtube_message(message):
+    bot.send_message(message.chat.id, 'Now this function unavailable❌\n\nSorry 😓😓😓\n\nBut you can download Instagram photos!\nJust tap: /help')
+
+
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Привет, мой создатель')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Прощай, создатель')
+    bot.send_photo(message.chat.id, open('Sorry.png', 'rb'))
+    bot.send_message(message.chat.id, 'Please, contact administrator or tap: /help')
 
 
 bot.polling()
